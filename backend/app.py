30250ff1fcff697a48ad6cf1a50afba791fd6c72
laharimorampudi22 <2320030306@klh.edu.in>
2025-10-30 10:21:10 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,25 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+import numpy as np
+from model import predict_future
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Climate Change Predictor API Running ✅"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        years = int(data.get('years', 1))
+        prediction = predict_future(years)
+        return jsonify(prediction)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
