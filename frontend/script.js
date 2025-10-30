9c6d94d1e4077c4610b5277f83b04057f4cdd0ea
laharimorampudi22 <2320030306@klh.edu.in>
2025-10-30 10:22:17 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,11 @@
+async function getPrediction() {
+  const years = document.getElementById('years').value;
+  const response = await fetch('http://127.0.0.1:5000/predict', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({years: years})
+  });
+  const data = await response.json();
+  document.getElementById('output').innerText = JSON.stringify(data, null, 2);
+}
 
